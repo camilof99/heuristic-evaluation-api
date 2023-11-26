c7036859fe1065fcdc5b103f702888c66fe8cbdf
Camilo Faria <camilozabaleta65@gmail.com>
2023-11-26 13:47:45 -0500

    Update query project

--- src/server.js
+++ src/server.js
@@ -59,7 +59,8 @@ app.post("/api/login", async (req, res) => {
 });
 
 app.get("/api/projects", async (req, res) => {
-    const query = "SELECT projects.*, evaluator.name AS evaluator_name, coordinator.name AS coordinator_name FROM projects LEFT JOIN users AS evaluator ON projects.id_evaluator = evaluator.id LEFT JOIN users AS coordinator ON projects.id_coordinator = coordinator.id";
+    const query =
+        "SELECT projects.*, evaluator.name AS evaluator_name, coordinator.name AS coordinator_name FROM projects LEFT JOIN users AS evaluator ON projects.id_evaluator = evaluator.id LEFT JOIN users AS coordinator ON projects.id_coordinator = coordinator.id order by id asc;";
 
     client.query(query, (error, results) => {
         if (error) {
